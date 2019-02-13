963a528c150a0ecff6bd3b692c14d56456c6b3eb
Frédéric Sallin <frederic.sallin@etu.unige.ch>
2019-02-13 13:00:29 +0100

    edit eval lstm

--- im2txt_LSTM/evaluation_lstm.sh
+++ im2txt_LSTM/evaluation_lstm.sh
@@ -14,22 +14,15 @@ module load GCC/4.9.3-2.25 OpenMPI/1.10.2 Python/2.7.11 foss/2016a Java/1.8.0_92
 # if you need to know the allocated CUDA device, you can obtain it here:
 echo $CUDA_VISIBLE_DEVICES
 
-PROJ_BACH="~/proj_bach"
-MSCOCO_DIR="${PROJ_BACH}/data/mscoco"
-MODEL_DIR="${PROJ_BACH}/im2txt_LSTM/im2txt/model"
-
 cd $PROJ_BACH
 source python_virtual_env/bin/activate
 cd im2txt_LSTM
 
 /opt/bazel/bazel-0.4.4/bazel build -c opt //im2txt/evaluate
 
-
-
-
 # Run the evaluation script. This will run in a loop, periodically loading the
 # latest model checkpoint file and computing evaluation metrics.
 bazel-bin/im2txt/evaluate \
-  --input_file_pattern="${MSCOCO_DIR}/val-?????-of-00004" \
-  --checkpoint_dir="${MODEL_DIR}/train_12h" \
-  --eval_dir="${MODEL_DIR}/eval_12h"
+  --input_file_pattern=$HOME/proj_bach/data/mscoco/val-?????-of-00004 \
+  --checkpoint_dir=$HOME/proj_bach/im2txt_LSTM/im2txt/model/train_12h \
+  --eval_dir=$HOME/proj_bach/im2txt_LSTM/im2txt/model/eval_12h
