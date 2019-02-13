e72f978ef2e7f3b932a9adcc06394fcbe02f121a
Frédéric Sallin <frederic.sallin@etu.unige.ch>
2019-02-13 12:27:35 +0100

    eval lstm

---
+++ im2txt_LSTM/evaluation_lstm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -J evaluation_LSTM
+#SBATCH -o output/evaluation_LSTM.o%j
+#SBATCH --constraint="V5|V6"
+#SBATCH --partition=shared-gpu
+#SBATCH --time=00:15:00
+#SBATCH --gres=gpu:titan:2
+#SBATCH --mem=24GB
+
+module purge
+module load GCC/4.9.3-2.25 OpenMPI/1.10.2 Python/2.7.11 foss/2016a Java/1.8.0_92 cuDNN/5.1-CUDA-8.0.44 tensorflow/1.3.0-Python-2.7.11
+
+# if you need to know the allocated CUDA device, you can obtain it here:
+echo $CUDA_VISIBLE_DEVICES
+
+PROJ_BACH = "~/proj_bach/"
+MSCOCO_DIR="${PROJ_BACH}/data/mscoco"
+MODEL_DIR="${PROJ_BACH}/im2txt_LSTM/im2txt/model"
+
+cd $PROJ_BACH
+source python_virtual_env/bin/activate
+cd im2txt_LSTM
+
+/opt/bazel/bazel-0.4.4/bazel build -c opt //im2txt/evaluate
+
+
+
+
+# Run the evaluation script. This will run in a loop, periodically loading the
+# latest model checkpoint file and computing evaluation metrics.
+bazel-bin/im2txt/evaluate \
+  --input_file_pattern="${MSCOCO_DIR}/val-?????-of-00004" \
+  --checkpoint_dir="${MODEL_DIR}/train_12h" \
+  --eval_dir="${MODEL_DIR}/eval_12h"
---
+++ im2txt_LSTM/maison_evaluation_lstm.sh
@@ -0,0 +1,16 @@
+MSCOCO_DIR="/mnt/d/data/mscoco"
+MODEL_DIR="${HOME}/proj_bach/im2txt_LSTM/im2txt/model"
+
+# Ignore GPU devices (only necessary if your GPU is currently memory
+# constrained, for example, by running the training script).
+#export CUDA_VISIBLE_DEVICES=""
+
+bazel build -c opt //im2txt:evaluate
+
+
+# Run the evaluation script. This will run in a loop, periodically loading the
+# latest model checkpoint file and computing evaluation metrics.
+bazel-bin/im2txt/evaluate \
+  --input_file_pattern="${MSCOCO_DIR}/val-?????-of-00004" \
+  --checkpoint_dir="${MODEL_DIR}/train_12h" \
+  --eval_dir="${MODEL_DIR}/eval_12h"
